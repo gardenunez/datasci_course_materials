322f58986f1f711d423e5f5a100a974fffc98651
jorge jardines <gardenunez@gmail.com>
2014-07-06 02:17:31 +0200

    start parsing the tweet file

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,29 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
+def hw(tweet_file):
+    tweets = parse_tweets(tweet_file)
+    print len(tweets)
 
 def lines(fp):
     print str(len(fp.readlines()))
 
+def parse_tweets(source_file):
+    lines = []
+    for line in source_file.readlines():
+        lines.append(json.loads(line))
+    return lines
+
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    try:
+        sent_file = open(sys.argv[1])
+        tweet_file = open(sys.argv[2])
+        hw(tweet_file)
+        lines(sent_file)
+        #lines(tweet_file)
+    finally:
+        sent_file.close()
+        tweet_file.close()
 
 if __name__ == '__main__':
     main()
