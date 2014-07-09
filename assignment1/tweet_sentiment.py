dd4d26d0957e778e46a76774d3c7231b7a881e82
jorge jardines <gardenunez@gmail.com>
2014-07-09 21:51:14 +0200

    parse sentiment file and tweet file

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -5,22 +5,30 @@ def hw(tweet_file):
     tweets = parse_tweets(tweet_file)
     print len(tweets)
 
-def lines(fp):
-    print str(len(fp.readlines()))
-
 def parse_tweets(source_file):
     lines = []
     for line in source_file.readlines():
         lines.append(json.loads(line))
     return lines
 
+def parse_sentiment_file(sent_file):
+    """
+    Parse the sentiment file.
+    return: dict
+    """
+    scores = {} # initialize an empty dictionary
+    for line in sent_file:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  # Convert the score to an integer.
+    return scores
+
 def main():
     try:
         sent_file = open(sys.argv[1])
         tweet_file = open(sys.argv[2])
-        hw(tweet_file)
-        lines(sent_file)
-        #lines(tweet_file)
+        scores = parse_sentiment_file(sent_file)
+        tweets = parse_tweets(tweet_file)
+        print tweets
     finally:
         sent_file.close()
         tweet_file.close()
